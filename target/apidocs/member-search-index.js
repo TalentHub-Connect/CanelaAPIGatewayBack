46b044f1440fac979223b4833fbd3851c8b939c8
Wilermo <william.dmo.sec@gmail.com>
2024-04-17 10:01:38 -0500

    Corrección configuración CORS

---
+++ target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.talenthub.apigatewaycanela","c":"ApigatewaycanelaApplication","l":"ApigatewaycanelaApplication()","u":"%3Cinit%3E()"},{"p":"com.talenthub.apigatewaycanela","c":"ApigatewaycanelaApplication","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
